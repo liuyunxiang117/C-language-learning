cd22864ac02dad29e536a6042302dd238f762047
liuyunxiang117 <3387753478@qq.com>
2026-09-11 23:02:55 +0800

    first C learning

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    // printf("%s","abc\ndef");
+    // printf("%d",123);
+    // printf("abc\rcde");
+    printf("hello");
+    printf("\r");
+    printf("world");
+    return 0;
+}
